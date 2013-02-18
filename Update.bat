0e94c86d4c77d379b7f2af01be4e6d232f6089a5
Andrew K <andrewjk@server.fake>
2013-02-18 20:15:44 +1100

    Removed some files

--- Update.bat
+++
@@ -1,6 +0,0 @@
-xcopy ..\Watsonia\Watsonia.Data\*.* Watsonia.Data\ /s /y /d /exclude:exclude.txt
-xcopy ..\Watsonia\Watsonia.Data.SqlServer\*.* Watsonia.Data.SqlServer\ /s /y /d /exclude:exclude.txt
-xcopy ..\Watsonia\Watsonia.Data.SqlServerCe\*.* Watsonia.Data.SqlServerCe\ /s /y /d /exclude:exclude.txt
-xcopy ..\Watsonia\Watsonia.Data.Tests\*.* Watsonia.Data.Tests\ /s /y /d /exclude:exclude.txt
-
-pause
