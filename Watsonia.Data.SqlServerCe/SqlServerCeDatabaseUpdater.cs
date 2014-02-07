b4d130e39ab82c47b7dba9a7e39a7d891939dfc7
Andrew K <andrewjk@server.fake>
2014-02-07 16:59:56 +1100

    More support for the Compact Edition

--- Watsonia.Data.SqlServerCe/SqlServerCeDatabaseUpdater.cs
+++ Watsonia.Data.SqlServerCe/SqlServerCeDatabaseUpdater.cs
@@ -14,27 +14,8 @@ namespace Watsonia.Data.SqlServerCe
 		public SqlServerCeDatabaseUpdater(IDataAccessProvider dataAccessProvider, DatabaseConfiguration configuration)
 			: base(dataAccessProvider, configuration)
 		{
-			// Can't create clustered keys
-			this.ClusterKeys = false;
-		}
-
-		protected override Type FrameworkTypeFromDatabase(string databaseTypeName, bool allowNulls)
-		{
-			// Decimal is called numeric, apparently
-			if (databaseTypeName.ToUpperInvariant() == "NUMERIC")
-			{
-				return allowNulls ? typeof(decimal?) : typeof(decimal);
-			}
-			else
-			{
-				return base.FrameworkTypeFromDatabase(databaseTypeName, allowNulls);
-			}
-		}
-
-		protected override List<string> GetDefaultValueConstraintsToDrop(MappedTable table, MappedColumn column, DbConnection connection)
-		{
-			// Can't get default value constraints
-			return new List<string>();
+			// Can't create clustered keys among other things
+			this.CompactEdition = true;
 		}
 
 		protected override DbCommand CreateCommand(DbConnection connection)
