1bc380bf45a47fb039088a09122f3cacf4c2b650
Andrew K <andrewjk@gmail.com>
2016-05-03 12:46:52 +1000

    Explicitly select fields rather than *

--- Watsonia.Data/SelectStatementCreator.cs
+++ Watsonia.Data/SelectStatementCreator.cs
@@ -3,8 +3,10 @@ using Remotion.Linq.Clauses;
 using Remotion.Linq.Clauses.ResultOperators;
 using Remotion.Linq.Parsing.Structure;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using Watsonia.Data.Sql;
 
 namespace Watsonia.Data
@@ -60,7 +62,50 @@ namespace Watsonia.Data
 
 		public override void VisitSelectClause(SelectClause selectClause, QueryModel queryModel)
 		{
-			if (selectClause.Selector.NodeType != ExpressionType.Extension)
+			if (selectClause.Selector.NodeType == ExpressionType.Extension)
+			{
+				// If we are selecting an object, specify its fields
+				// This will avoid the case where selecting fields from multiple tables with non-unique field
+				// names (e.g. two tables with an ID field) fills the object with the wrong value
+				var columnNames = new List<string>();
+				string primaryKeyColumnName = this.Configuration.GetPrimaryKeyColumnName(selectClause.Selector.Type);
+				foreach (PropertyInfo property in this.Configuration.PropertiesToMap(selectClause.Selector.Type))
+				{
+					if (this.Configuration.IsRelatedItem(property))
+					{
+						// It's a property referencing another table so change its name and type
+						string columnName = this.Configuration.GetForeignKeyColumnName(property);
+						if (!columnNames.Any(c => c.Equals(columnName, StringComparison.InvariantCultureIgnoreCase)))
+						{
+							columnNames.Add(columnName);
+						}
+					}
+					else if (this.Configuration.IsRelatedCollection(property))
+					{
+						// It's a collection property referencing another table so ignore it
+					}
+					else
+					{
+						// It's a regular mapped column
+						string columnName = this.Configuration.GetColumnName(property);
+						if (!columnName.Equals(primaryKeyColumnName, StringComparison.InvariantCultureIgnoreCase) &&
+							!columnNames.Any(c => c.Equals(columnName, StringComparison.InvariantCultureIgnoreCase)))
+						{
+							columnNames.Add(columnName);
+						}
+					}
+				}
+
+				// Add the primary key column in the first position for nicety
+				columnNames.Insert(0, primaryKeyColumnName);
+
+				string tableName = this.Configuration.GetTableName(selectClause.Selector.Type);
+				foreach (string columnName in columnNames)
+				{
+					this.SelectStatement.SourceFields.Add(new Column(tableName, columnName));
+				}
+			}
+			else
 			{
 				StatementPart fields = StatementPartCreator.Visit(queryModel, selectClause.Selector, this.Configuration);
 				this.SelectStatement.SourceFields.Add((SourceExpression)fields);
@@ -209,6 +254,15 @@ namespace Watsonia.Data
 				else
 				{
 					throw new NotSupportedException("Currently not supporting methods or variables in the Skip or Take clause.");
+				}
+				return;
+			}
+
+			if (resultOperator is ReverseResultOperator)
+			{
+				foreach (OrderByExpression orderBy in this.SelectStatement.OrderByFields)
+				{
+					orderBy.Direction = (orderBy.Direction == OrderDirection.Ascending) ? OrderDirection.Descending : OrderDirection.Ascending;
 				}
 				return;
 			}
