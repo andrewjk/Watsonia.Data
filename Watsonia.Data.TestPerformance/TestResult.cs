27b1b426e12a85e406138df3f27c3ecbc3eef0a4
Andrew K <andrewjk@gmail.com>
2017-02-05 15:02:04 +1100

    Update .gitignore

---
+++ Watsonia.Data.TestPerformance/TestResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Watsonia.Data.TestPerformance
+{
+	public class TestResult
+	{
+		public int Number { get; set; }
+		public TestFramework Framework { get; set; }
+		public double AllPostsMilliseconds { get; set; }
+		public double PlayerByIDMilliseconds { get; set; }
+		public double PlayersForTeamMilliseconds { get; set; }
+		public double TeamsForSportMilliseconds { get; set; }
+	}
+}
